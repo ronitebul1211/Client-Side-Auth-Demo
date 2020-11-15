8906331b8eb7cbd8a64d19ac973e8f4e02874aa2
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-15 13:51:16 +0200

    implement auth0 on navbar

---
+++ src/components/Authentication.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { withAuth0 } from "@auth0/auth0-react";
+
+class AuthenticationButton extends React.Component {
+   render() {
+      const { isAuthenticated, logout, loginWithRedirect } = this.props.auth0;
+      return isAuthenticated ? (
+         <li
+            onClick={() => {
+               logout();
+            }}>
+            SIGN OUT
+         </li>
+      ) : (
+         <li
+            onClick={() => {
+               loginWithRedirect({});
+            }}>
+            SIGN IN
+         </li>
+      );
+   }
+}
+// Wrap class component in high order component that five it access to auth0context (e.g  line 7)
+export default withAuth0(AuthenticationButton);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,57 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { withAuth0 } from "@auth0/auth0-react";
+import Authentication from "./Authentication";
 
 class Navbar extends React.Component {
-   renderAppOptions = () => {
-      if (this.props.isAuthenticated) {
-         return (
-            <React.Fragment>
-               <li>
-                  <Link to="/dashboard">DASHBOARD</Link>
-               </li>
-               <li>
-                  <Link to="/settings">SETTINGS</Link>
-               </li>
-            </React.Fragment>
-         );
-      }
-   };
-
-   renderUserOptions = () => {
-      if (this.props.isAuthenticated) {
-         return (
-            <React.Fragment>
-               <li onClick={this.props.handleSignOut}>SIGN OUT</li>
-            </React.Fragment>
-         );
-      } else {
-         return (
-            <React.Fragment>
-               <li>
-                  <Link to="/sign-up">SIGN UP</Link>
-               </li>
-               <li>
-                  <Link to="/sign-in">SIGN IN</Link>
-               </li>
-            </React.Fragment>
-         );
-      }
-   };
-
    render() {
+      const { isAuthenticated } = this.props.auth0;
       return (
          <nav className="nav-bar">
             <ul>
                <li>
                   <Link to="/">MY APP LOGO</Link>
                </li>
-               {this.renderAppOptions()}
+               {isAuthenticated ? (
+                  <React.Fragment>
+                     <li>
+                        <Link to="/dashboard">DASHBOARD</Link>
+                     </li>
+                     <li>
+                        <Link to="/settings">SETTINGS</Link>
+                     </li>
+                  </React.Fragment>
+               ) : null}
             </ul>
-            <ul>{this.renderUserOptions()}</ul>
+            <ul>
+               <Authentication />
+            </ul>
          </nav>
       );
    }
 }
 
-export default Navbar;
+export default withAuth0(Navbar);
