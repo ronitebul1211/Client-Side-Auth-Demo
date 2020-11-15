10109ff7121223b8c6d567f2fd4dfac8a478db24
ronitebul1211 <ronitebul1211@gmail.com>
2020-11-15 12:33:59 +0200

    add auth0 provider

--- src/index.js
+++ src/index.js
@@ -3,9 +3,16 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
+//Auth0
+import { Auth0Provider } from "@auth0/auth0-react";
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
+const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 ReactDOM.render(
    <BrowserRouter>
-      <App />
+      <Auth0Provider domain={domain} clientId={clientId} redirectUri={window.location.origin}>
+         <App />
+      </Auth0Provider>
    </BrowserRouter>,
    document.getElementById("root"),
 );
